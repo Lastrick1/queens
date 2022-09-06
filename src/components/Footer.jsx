67ce923aec6a0f956358d1c2263d6ee0e33d8125
Andy Strickland <53843216+Lastrick1@users.noreply.github.com>
2022-09-06 14:28:36 -0400

    Update footer to include name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright Andy Strickland ⓒ {year}</p>
     </footer>
   );
 }
